a199e61dea9a52c88888ba02aab67f38ac52f1c9
Aleksandr Borodin <aleksandr_borodin@senla.eu>
2024-02-28 17:49:58 +0300

    suppressed warnings

--- src/main/java/com/lisko/exception/AuthorizationException.java
+++ src/main/java/com/lisko/exception/AuthorizationException.java
@@ -1,16 +1,13 @@
 package com.lisko.exception;
 
 import com.lisko.enums.ResponseCode;
-import org.springframework.lang.Nullable;
-
-import java.util.Objects;
 
 /**
  * Author: Aleksandr Borodin
  * Creation date: 2/2/24
  */
 public class AuthorizationException extends BaseRequestException{
-    public AuthorizationException(@Nullable Object payload) {
-        super(ResponseCode.AUTH_FAILED, !Objects.isNull(payload) ? "Authorization failed for " + payload : null);
+    public AuthorizationException(Object payload) {
+        super(ResponseCode.AUTH_FAILED, "Authorization failed for " + payload);
     }
 }
